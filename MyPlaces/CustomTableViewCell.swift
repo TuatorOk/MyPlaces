12afdf7f8d62cbacb544631b834d2e3de50475a7
Volodymyr Sklyarov <vskl3569@gmail.com>
2022-11-22 22:48:07 +0200

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Macbook on 11/22/22.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+    
+    
+    
+}
